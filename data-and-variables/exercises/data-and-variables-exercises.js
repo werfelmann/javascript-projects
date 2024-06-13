5b9d3b85ce9b028a3d594836212ba96152b125fc
David Werfelmann <werfelmann@Davids-MBP.attlocal.net>
2024-06-13 12:49:31 -0500

    completed data-and-variables exercise: cacluate space mission

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,32 @@
 // Declare and assign the variables below
+let shuttleName = "Determination";
+let shuttleSpeedMph = 17500;
+let marsDistanceKm = 225000000;
+let moonDistanceKm = 384400;
+const milesPerKm = 0.621;
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof marsDistanceKm);
+console.log(typeof moonDistanceKm);
+console.log(typeof milesPerKm);
 
 // Calculate a space mission below
+let milesToMars = marsDistanceKm * milesPerKm;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
 
 // Print the results of the space mission calculations below
 
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.");
+
 // Calculate a trip to the moon below
 
+let milesToMoon = moonDistanceKm * milesPerKm;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+
 // Print the results of the trip to the moon below
+
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.")
