db16df67422682d6be378cced049434a93609fd5
David Werfelmann <werfelmann@Davids-MBP.attlocal.net>
2024-07-18 10:20:04 -0500

    chapter 17 exercises complete

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,12 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    try {
+      let lab = labs[i];
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    } catch(err) {
+      console.log("Error thrown");
+    }
   }
 }
 
@@ -21,4 +25,25 @@ let studentLabs = [
   }
 ];
 
-gradeLabs(studentLabs);
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
