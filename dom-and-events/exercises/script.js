fb6a5f57811c3315513ab593a1ff6063764f12c5
David Werfelmann <werfelmann@Davids-MBP.attlocal.net>
2024-08-04 22:16:10 -0500

    chapter 23 exercises plus examples

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,30 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+
+    function liftoff() {
+        document.getElementById("statusReport").innerHTML = "Houston, we have liftoff!";
+        console.log("Houston, we have liftoff!")
+    }
+
+    button.addEventListener("click", liftoff);
+
+    missionAbort.addEventListener("mouseover", function(event) {
+        missionAbort.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function(event) {
+        missionAbort.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function(event) {
+        let confirmation = window.confirm("Are you sure you want to abort the mission?");
+        if (confirmation) {
+            document.getElementById("statusReport").innerHTML = "Mission aborted! Space shuttle returning home.";
+            console.log("Mission aborted! Space shuttle returning home.");
+        }
+    });
+    
     
 }
 
