7977081a15724fc18c7320f4adb7666d37129617
David Werfelmann <werfelmann@Davids-MBP.attlocal.net>
2024-08-08 17:17:41 -0500

    ch 25 exercises
    exit
    e

--- user-input-with-forms/chapter-examples/form-validation/script.js
+++ user-input-with-forms/chapter-examples/form-validation/script.js
@@ -1,1 +1,8 @@
-//Add Your Code Below
+    window.addEventListener("load", function() {
+        let form = document.querySelector("form");
+        form.addEventListener("submit", function(event) {
+        let usernameInput = document.querySelector("input[name=username]");
+        // alert the current value found in the username input
+        alert("username: " + usernameInput.value);
+        });
+    });
--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,4 @@
 //Code Your Solution Below
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {})})
