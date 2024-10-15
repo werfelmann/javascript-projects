60e62b4937db306f2c8aba4a9b1cda2b46778dee
werfelmann <werfelmann@gmail.com>
2024-10-15 11:22:51 -0500

    10/20 string methods

---
+++ sandbox/jsPractice/jsPractice.js
@@ -0,0 +1,10 @@
+const str1 = "The Universe is under no obligation to make sense to you.";
+const str2 = "That's one small step for a man, one giant leap for mankind.";
+const str3 = "Space is to place as eternity is to time.";
+const char = "0"
+
+
+
+console.log(str1.toUpperCase());
+console.log(str2.toLowerCase());
+console.log
